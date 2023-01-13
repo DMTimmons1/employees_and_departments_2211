b5a89a5bad0858106bdfa8601947aa5ffae908b3
Dawson Timmons <DawsonTimmons@gmail.com>
2023-01-13 09:33:14 -0700

    Add hire method in department class and passing test

--- spec/department_spec.rb
+++ spec/department_spec.rb
@@ -3,6 +3,8 @@ require './lib/employee'
 
 RSpec.describe Department do
   let(:customer_service) { Department.new("Customer Service") }
+  let(:bobbi) { Employee.new ( { name: "Bobbi Jaeger", age: "30", salary: "$100000" } ) }
+  let(:aaron) { Employee.new ( { name: "Aaron Tanaka", age: "25", salary: "90000" } ) }  
 
   describe "#initialize" do
     it "exists" do
@@ -12,6 +14,18 @@ RSpec.describe Department do
     it "has readable attributes" do
       expect(customer_service.name).to eq("Customer Service")
       expect(customer_service.employees).to eq([])
+    end
+  end
+  
+
+  describe "#hire" do
+  
+    it "adds employees to our employees array" do
+    
+      customer_service.hire(bobbi)
+      customer_service.hire(aaron)
+
+      expect(customer_service.employees).to eq([bobbi, aaron])
     end
   end
 end
