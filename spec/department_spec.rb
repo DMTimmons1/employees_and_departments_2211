c8cdf4d281b959fc097a3b48d7423ef17f15858b
Dawson Timmons <DawsonTimmons@gmail.com>
2023-01-13 09:21:50 -0700

    Add the department class and initialize test

---
+++ spec/department_spec.rb
@@ -0,0 +1,12 @@
+require './lib/department'
+require './lib/employee'
+
+RSpec.describe Department do
+  let(:customer_service) { Department.new("Customer Service") }
+
+  describe "#initialize" do
+    it "exists" do
+      expect(customer_service).to be_an_instance_of(Department)
+    end
+  end
+end
