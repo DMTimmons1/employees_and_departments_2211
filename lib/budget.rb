fab6065fc18d2e8d9836af330c543c68cf27546b
Dawson Timmons <DawsonTimmons@gmail.com>
2023-01-13 10:21:07 -0700

    Add low_expense_departments method and passing test

--- lib/budget.rb
+++ lib/budget.rb
@@ -4,9 +4,19 @@ class Budget
   def initialize(year)
     @year = year
     @departments = []
+    @departments_under_500 = []
   end
 
   def add_department(department)
     @departments << department
+  end
+
+  def low_expense_departments
+    departments.each do |department|
+      if department.expenses <= 500
+        @departments_under_500 << department
+      end
+    end
+    return @departments_under_500
   end
 end
--- spec/budget_spec.rb
+++ spec/budget_spec.rb
@@ -5,6 +5,8 @@ require './lib/employee'
 RSpec.describe Budget do
   let(:yearly_budget) { Budget.new(2023)}
   let(:customer_service) { Department.new("Customer Service") }
+  let(:sales) { Department.new("Sales") }
+  let(:human_resources) { Department.new("Sales") }
   
   describe "#initialize" do
     it "exists" do
@@ -25,6 +27,20 @@ RSpec.describe Budget do
       yearly_budget.add_department(customer_service)
 
       expect(yearly_budget.departments).to eq([customer_service])
+    end
+  end
+
+  describe "#low_expense_departments" do
+    it "shows all the departments with less than $500 in expenses" do
+      yearly_budget.add_department(customer_service)
+      yearly_budget.add_department(sales)
+      yearly_budget.add_department(human_resources)
+
+      customer_service.expense(250)
+      sales.expense(775)
+      human_resources.expense(100)
+
+      expect(yearly_budget.low_expense_departments).to eq([customer_service, human_resources])
     end
   end
 end
