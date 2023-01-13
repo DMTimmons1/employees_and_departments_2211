e9bdd88e0321fd1ec3fdc5e0b98ff4dd61ca191c
Dawson Timmons <DawsonTimmons@gmail.com>
2023-01-13 09:12:40 -0700

    Add attributes to initialize and test

--- spec/employee_spec.rb
+++ spec/employee_spec.rb
@@ -7,5 +7,11 @@ RSpec.describe Employee do
     it "exists" do
       expect(bobbi).to be_an_instance_of(Employee)
     end
+
+    it "has readable attributes" do
+      expect(bobbi.name).to eq("Bobbi Jaeger")
+      expect(bobbi.age).to eq(30)
+      expect(bobbi.salary).to eq(100000)
+    end
   end
 end
